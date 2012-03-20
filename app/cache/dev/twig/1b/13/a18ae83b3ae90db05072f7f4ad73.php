458f2a3c1a2d5a7c8a75e2650ef0e502a34c626c
Ivan Shentov <shentov@gmail.com>
2012-03-20 19:36:22 +0200

    Mysql configured
    Little config fixes
    
    Signed-off-by: Ivan Shentov <shentov@gmail.com>

---
+++ app/cache/dev/twig/1b/13/a18ae83b3ae90db05072f7f4ad73.php
@@ -0,0 +1,21 @@
+<?php
+
+/* TwigBundle:Exception:error.atom.twig */
+class __TwigTemplate_1b13a18ae83b3ae90db05072f7f4ad73 extends Twig_Template
+{
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        // line 1
+        $this->env->loadTemplate("TwigBundle:Exception:error.xml.twig")->display(array_merge($context, array("exception" => $this->getContext($context, "exception"))));
+    }
+
+    public function getTemplateName()
+    {
+        return "TwigBundle:Exception:error.atom.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+}
---
+++ app/cache/dev/twig/4e/c5/bf758c14550bd7d539ba7feca7d4.php
@@ -0,0 +1,34 @@
+<?php
+
+/* TwigBundle:Exception:error.txt.twig */
+class __TwigTemplate_4ec5bf758c14550bd7d539ba7feca7d4 extends Twig_Template
+{
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        // line 1
+        echo "Oops! An Error Occurred
+=======================
+
+The server returned a \"";
+        // line 4
+        echo twig_escape_filter($this->env, $this->getContext($context, "status_code"), "html", null, true);
+        echo " ";
+        echo twig_escape_filter($this->env, $this->getContext($context, "status_text"), "html", null, true);
+        echo "\".
+
+Please e-mail us at [email] and let us know what you were doing when this
+error occurred. We will fix it as soon as possible. Sorry for any
+inconvenience caused.
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "TwigBundle:Exception:error.txt.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+}
